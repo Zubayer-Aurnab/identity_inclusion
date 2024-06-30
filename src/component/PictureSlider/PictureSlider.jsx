07ddbdc0faf98765bfc33c6e8e05d896a5f55141
Aurnab <zubayeraurnab@gmail.com>
2024-06-30 15:20:44 +0600

    slider picture section created on the home page

--- src/component/PictureSlider/PictureSlider.jsx
+++ src/component/PictureSlider/PictureSlider.jsx
@@ -14,29 +14,74 @@ import img12 from '../../assets/marquee/img12.jpg'
 import img13 from '../../assets/marquee/img13.jpg'
 import img14 from '../../assets/marquee/img14.jpg'
 import img15 from '../../assets/marquee/img15.jpg'
+import img16 from '../../assets/marquee/img16.jpg'
+import img17 from '../../assets/marquee/img17.jpg'
+import img18 from '../../assets/marquee/img18.jpg'
+import img19 from '../../assets/marquee/img19.jpg'
+import img20 from '../../assets/marquee/img20.jpg'
+import img21 from '../../assets/marquee/img21.jpg'
+import img22 from '../../assets/marquee/img22.jpg'
+import img23 from '../../assets/marquee/img23.jpg'
+import img24 from '../../assets/marquee/img24.jpg'
+import img25 from '../../assets/marquee/img25.jpg'
+import img26 from '../../assets/marquee/img26.jpg'
+import img27 from '../../assets/marquee/img27.jpg'
+import img28 from '../../assets/marquee/img28.jpg'
+import img29 from '../../assets/marquee/img29.jpg'
+import Title from "../Title/Title";
+import { useEffect } from "react";
+import Aos from "aos";
 
 const PictureSlider = () => {
-    const imageClass = "h-64 mx-5"
+    const imageClass = "h-64 w-full object-contain lg lg:h-64 mx-3"
+    useEffect(() => {
+        Aos.init()
+    }, [])
     return (
-        <div className="my-10 w-4/5 mx-auto">
-            <Marquee pauseOnHover direction="right" >
-                <img className={imageClass} src={img1} alt="" />
-                <img className={imageClass} src={img2} alt="" />
-                <img className={imageClass} src={img3} alt="" />
-                <img className={imageClass} src={img4} alt="" />
-                <img className={imageClass} src={img5} alt="" />
-                <img className={imageClass} src={img6} alt="" />
-                <img className={imageClass} src={img7} alt="" />
-                <img className={imageClass} src={img8} alt="" />
-                <img className={imageClass} src={img9} alt="" />
-                <img className={imageClass} src={img10} alt="" />
-                <img className={imageClass} src={img11} alt="" />
-                <img className={imageClass} src={img12} alt="" />
-                <img className={imageClass} src={img13} alt="" />
-                <img className={imageClass} src={img14} alt="" />
-                <img className={imageClass} src={img15} alt="" />
-            </Marquee>
-        </div>
+        <>
+            <div data-aos="fade-up" data-aos-duration="2000">
+                <div className="my-20 px-2 lg:px-0 lg:w-4/5 mx-auto ">
+                    <Title>
+                        Activities
+                    </Title>
+                </div>
+                <div className="my-10 px-2 lg:px-0 lg:w-4/5 mx-auto space-y-5">
+                    <Marquee pauseOnHover direction="right" >
+                        <img className={imageClass} src={img1} alt="" />
+                        <img className={imageClass} src={img2} alt="" />
+                        <img className={imageClass} src={img3} alt="" />
+                        <img className={imageClass} src={img4} alt="" />
+                        <img className={imageClass} src={img5} alt="" />
+                        <img className={imageClass} src={img6} alt="" />
+                        <img className={imageClass} src={img7} alt="" />
+                        <img className={imageClass} src={img8} alt="" />
+                        <img className={imageClass} src={img9} alt="" />
+                        <img className={imageClass} src={img10} alt="" />
+                        <img className={imageClass} src={img11} alt="" />
+                        <img className={imageClass} src={img12} alt="" />
+                        <img className={imageClass} src={img13} alt="" />
+                        <img className={imageClass} src={img14} alt="" />
+                        <img className={imageClass} src={img15} alt="" />
+                    </Marquee>
+                    <Marquee pauseOnHover direction="left" >
+                        <img className={imageClass} src={img16} alt="" />
+                        <img className={imageClass} src={img17} alt="" />
+                        <img className={imageClass} src={img18} alt="" />
+                        <img className={imageClass} src={img19} alt="" />
+                        <img className={imageClass} src={img20} alt="" />
+                        <img className={imageClass} src={img21} alt="" />
+                        <img className={imageClass} src={img22} alt="" />
+                        <img className={imageClass} src={img23} alt="" />
+                        <img className={imageClass} src={img24} alt="" />
+                        <img className={imageClass} src={img25} alt="" />
+                        <img className={imageClass} src={img26} alt="" />
+                        <img className={imageClass} src={img27} alt="" />
+                        <img className={imageClass} src={img28} alt="" />
+                        <img className={imageClass} src={img29} alt="" />
+                    </Marquee>
+                </div>
+            </div>
+        </>
     );
 };
 
